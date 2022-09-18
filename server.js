01912eeca86423fed150e3f1e8b9339bd65117a5
ErmLxmn <73292532+ErmLxmn@users.noreply.github.com>
2022-09-18 21:20:52 +0800

    final try 3

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require('express')
-
+const path = require('path')
 const app = express()
 
 //using public folder
 app.use(express.static(__dirname + '/public'))
 
+const views = path.join(__dirname, '/public/views/')
+
 app.get('/', function (req, res) {
-  res.sendFile('/views/index.html')
+  res.sendFile(render('index.html'))
 })
+
+// functions
+function render(view) {
+  let directory = path.join(views, view)
+  return directory
+}
 
 //defining port
 const PORT = process.env.PORT || 3000
