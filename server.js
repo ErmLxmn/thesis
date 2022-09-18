b08c50de455cf05a7bf4f71703794f9b37fa2313
ErmLxmn <73292532+ErmLxmn@users.noreply.github.com>
2022-09-18 21:08:18 +0800

    final try 1

--- server.js
+++ server.js
@@ -5,11 +5,13 @@ const app = express()
 //using public folder
 app.use(express.static(__dirname + '/public'))
 //setting view engine
-app.set('view-engine', 'ejs')
-app.set('views', 'public/views')
+// app.set('view-engine', 'ejs')
+// app.set('views', 'public/views')
 
 app.get('/', function (req, res) {
-  res.render('index.ejs')
+  res.json({
+    Hello: 'World',
+  })
 })
 
 //defining port
